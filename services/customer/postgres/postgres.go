e3555ee42034e21ad097a15143a3788e709851fb
selfscrfc <peachtre3@yandex.ru>
2023-12-04 19:51:50 +0300

    .

--- services/customer/postgres/postgres.go
+++ services/customer/postgres/postgres.go
@@ -5,20 +5,27 @@ import (
 	"github.com/jmoiron/sqlx"
 	"github.com/opentracing/opentracing-go"
 	"github.com/selfscrfc/PetBank/api/models"
+	"github.com/selfscrfc/PetBank/utils"
 )
 
 type customerRepo struct {
 	db *sqlx.DB
 }
+)
 
 func newCustomerRepo(db *sqlx.DB) *customerRepo {
 	return &customerRepo{db: db}
 }
 
-func Create(ctx context.Context, customer *models.Customer) {
+
+func  (db *customerRepo) Create(ctx context.Context, customer *models.Customer) {
 	span, ctx := opentracing.StartSpanFromContext(ctx, "newsRepo.Create")
 	defer span.Finish()
 
 	customer := models.Customer{}
-
+	utils.PostgreSQLConnection()
+	db, err := utils
+	if err != nil {
+		return nil, err
+	}
 }
